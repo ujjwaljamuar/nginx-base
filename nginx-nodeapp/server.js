069b9cf4c0b29d54ed2285f6a71fa19fc378abf1
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-02-20 18:02:12 +0530

    created reverse proxy server for a nodejs backend

---
+++ nginx-nodeapp/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = 3000;
+
+const appName = process.env.APP_NAME;
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  console.log(`request served by ${appName}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`${appName} is listening on port - ${PORT}`);
+});
